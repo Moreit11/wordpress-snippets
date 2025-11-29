475554be27ac7aa7a34c65c8dfb11c71ee1214fd
Moritz Reitz <110901281+Moreit11@users.noreply.github.com>
2025-11-29 18:10:16 +0000

    reference: register custom functions bricks builder

--- bricks-builder/register-custom-functions.php
+++ bricks-builder/register-custom-functions.php
@@ -1,5 +1,5 @@
 <?php
-// register your functions to enable them in bricks builder
+// register your functions to enable them in bricks builder https://academy.bricksbuilder.io/article/filter-bricks-code-echo_function_names/
 add_filter( 'bricks/code/echo_function_names', function() {
   return [
     'custom_function_name_1',
